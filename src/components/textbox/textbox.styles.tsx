678085db896d1300da4d79780a3f1662e34552b6
Oluwasegun <arpeiks@gmail.com>
2021-02-09 20:45:09 +0100

    chat box

---
+++ src/components/textbox/textbox.styles.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Root = styled.div`
+  padding: 16px;
+  margin-top: auto;
+  background-color: #fff;
+  border: 1px solid rgb(0, 0, 0, 0.1);
+`;
